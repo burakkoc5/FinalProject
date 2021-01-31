ca3ef258dda0fa2a5cdd0a0e4309b840aaa52dd1
Burak Koç <burak.koc@tedu.edu.tr>
2021-01-31 18:19:23 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden her class bir veri tabanı nesnesidir demek.
+    public interface IEntity
+    {
+
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class Kalmasın --> ÇÇK
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
